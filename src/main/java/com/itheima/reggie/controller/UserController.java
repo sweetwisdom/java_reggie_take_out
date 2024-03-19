f2bc73b4dc65501fedff641fa14094aab4ee6d01
zhangchao <sweetwisdom@qq.com>
2024-03-19 15:39:41 +0800

    添加 eventsouce支持

--- src/main/java/com/itheima/reggie/controller/UserController.java
+++ src/main/java/com/itheima/reggie/controller/UserController.java
@@ -4,18 +4,19 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.User;
 import com.itheima.reggie.service.UserService;
-import com.itheima.reggie.utils.SMSUtils;
 import com.itheima.reggie.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 @RequestMapping("/user")
@@ -27,24 +28,25 @@ public class UserController {
 
     /**
      * 发送手机短信验证码
+     *
      * @param user
      * @return
      */
     @PostMapping("/sendMsg")
-    public R<String> sendMsg(@RequestBody User user, HttpSession session){
+    public R<String> sendMsg(@RequestBody User user, HttpSession session) {
         //获取手机号
         String phone = user.getPhone();
 
-        if(StringUtils.isNotEmpty(phone)){
+        if (StringUtils.isNotEmpty(phone)) {
             //生成随机的4位验证码
             String code = ValidateCodeUtils.generateValidateCode(4).toString();
-            log.info("code={}",code);
+            log.info("code={}", code);
 
             //调用阿里云提供的短信服务API完成发送短信
             //SMSUtils.sendMessage("瑞吉外卖","",phone,code);
 
             //需要将生成的验证码保存到Session
-            session.setAttribute(phone,code);
+            session.setAttribute(phone, code);
 
             return R.success("手机验证码短信发送成功");
         }
@@ -54,12 +56,13 @@ public class UserController {
 
     /**
      * 移动端用户登录
+     *
      * @param map
      * @param session
      * @return
      */
     @PostMapping("/login")
-    public R<User> login(@RequestBody Map map, HttpSession session){
+    public R<User> login(@RequestBody Map map, HttpSession session) {
         log.info(map.toString());
 
         //获取手机号
@@ -72,24 +75,63 @@ public class UserController {
         Object codeInSession = session.getAttribute(phone);
 
         //进行验证码的比对（页面提交的验证码和Session中保存的验证码比对）
-        if(codeInSession != null && codeInSession.equals(code)){
+        if (codeInSession != null && codeInSession.equals(code)) {
             //如果能够比对成功，说明登录成功
 
             LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
-            queryWrapper.eq(User::getPhone,phone);
+            queryWrapper.eq(User::getPhone, phone);
 
             User user = userService.getOne(queryWrapper);
-            if(user == null){
+            if (user == null) {
                 //判断当前手机号对应的用户是否为新用户，如果是新用户就自动完成注册
                 user = new User();
                 user.setPhone(phone);
                 user.setStatus(1);
                 userService.save(user);
             }
-            session.setAttribute("user",user.getId());
+            session.setAttribute("user", user.getId());
             return R.success(user);
         }
         return R.error("登录失败");
     }
+
+    //    发送sse 消息
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private Runnable runnable;
+
+
+    @GetMapping(value = "/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @CrossOrigin
+    public SseEmitter handleSse() {
+        SseEmitter sseEmitter = new SseEmitter();
+        runnable = () -> {
+            try {
+                for (int i = 0; true; i++) {
+                    sseEmitter.send(SseEmitter.event().name("message").data("Message" + i));
+                    Thread.sleep(1000);
+
+                }
+            } catch (IOException | InterruptedException e) {
+
+                log.error("Error", e);
+                sseEmitter.completeWithError(e);
+            } finally {
+                sseEmitter.complete();
+            }
+
+        };
+        executor.execute(runnable);
+
+        return sseEmitter;
+    }
+
+    public void destroy() {
+        if (runnable != null && !executor.isShutdown()) {
+            runnable.run(); // 取消当前运行的任务
+        }
+        executor.shutdown();
+        log.info("close ⚠️");
+    }
+
 
 }
